e08dfcc7cfc0f7702dee7b2777c9527f67c9c9d1
Oyindamola Abolaji <78956440+oyin-systems@users.noreply.github.com>
2022-07-13 01:13:12 +0000

    check challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,10 +14,18 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let length = arrayOfNumbers.length;
+  let i = 0;
+  let sum = 0;
 
+  while (i < length)
+    {
+      sum += arrayOfNumbers[i];
+      i++;
+    }
+  return (sum);
+}
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -28,9 +36,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let length = arrayOfNumbers.length;
+  let i = 0;
+  let evenCount = 0;
+
+  while (i < length)
+    {
+      if(arrayOfNumbers[i] % 2 == 0)
+      {
+        evenCount++;
+      }
+      i++;
+    }
+  return (evenCount);
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
